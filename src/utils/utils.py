57b3632cc9105b4566075340abf7f12bf7c93275
yosxpeee <yosxpeee@gmail.com>
2026-03-12 01:38:54 +0900

    とりあえずCUIで作った処理移植

---
+++ src/utils/utils.py
@@ -0,0 +1,10 @@
+import requests
+HEADER = {
+    "Content-Type":"application/json", 
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+}
+
+def _doApi(url):
+    response = requests.get(url, headers=HEADER)
+    response.raise_for_status()  # エラー時にすぐ分かる
+    return response
