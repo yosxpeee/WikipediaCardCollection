94e52b234e8d52fcf8e8c5313ddcdb479bf8b08e
yosxpeee <yosxpeee@gmail.com>
2026-03-16 00:08:10 +0900

    不要ファイル削除

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from utils.utils import doApi, fetch_json
-import asyncio
-
-async def main():
-	ret = await fetch_json("https://ja.wikipedia.org/api/rest_v1/page/summary/%E8%B1%8A%E5%B3%B6%E3%83%BB%E3%82%B4%E3%82%A8%E3%81%AE%E3%81%82%E3%81%95%E3%81%AF%E3%82%84%E3%81%A3!")
-
-if __name__ == "__main__":
-	print("START")
-	asyncio.create_task(main())
